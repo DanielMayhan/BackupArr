783bcf9f8f1370aff96a46a5d4ee0cbed4d67980
DanielMayhan <muffin.game@protonmail.com>
2026-04-22 13:43:55 +0200

    changed name to backup.py

--- src/backup.py
+++ src/backup.py
@@ -8,7 +8,7 @@ url = api.baseurl + api.movielist + "?" + api.apikey
 
 ## Attempt Reconnection
 while True:
-    (established_connection, radarr_data) = functions.connectToRadarr(url)
+    (established_connection, radarr_data) = functions.connectToUrl(url, api.baseurl)
 
     if established_connection:
         print("Connection successful")
