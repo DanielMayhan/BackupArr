8e19f00448bc9e18bdd7b1c44f9162d97bb6dd0d
DanielMayhan <muffin.game@protonmail.com>
2026-06-02 15:05:48 +0200

    Removed unused colors

--- backuparr/utils/colors.py
+++ backuparr/utils/colors.py
@@ -1,10 +1,9 @@
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    OKBLUE = '\033[94m'         #BLUE
+    OKGREEN = '\033[92m'        #LIGHT GREEN
+    WARNING = '\033[93m'        #YELLOW
+    FAIL = '\033[91m'           #RED
+
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
