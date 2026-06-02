e246eff5ef479991d44ee7bb227ed1d184dbcaca
DanielMayhan <muffin.game@protonmail.com>
2026-06-02 15:07:00 +0200

    Changed names

--- backuparr/utils/colors.py
+++ backuparr/utils/colors.py
@@ -1,8 +1,8 @@
 class bcolors:
-    OKBLUE = '\033[94m'         #BLUE
-    OKGREEN = '\033[92m'        #LIGHT GREEN
-    WARNING = '\033[93m'        #YELLOW
-    FAIL = '\033[91m'           #RED
+    BLUE = '\033[94m'         #BLUE
+    GREEN = '\033[92m'        #LIGHT GREEN
+    YELLOW = '\033[93m'        #YELLOW
+    RED = '\033[91m'           #RED
 
     ENDC = '\033[0m'
     BOLD = '\033[1m'
