e8b1a9d12182a7e4b7680fbec1bc7200754ee820
DanielMayhan <muffin.game@protonmail.com>
2026-06-02 16:30:52 +0200

    Added more prints

--- backuparr/utils/printing.py
+++ backuparr/utils/printing.py
@@ -10,3 +10,9 @@ def printException(e):
 def printExit():
     print(f"{bcolors.RED}User terminated the process...{bcolors.ENDC}")
     sys.exit()
+
+def customExit(message):
+    sys.exit(f"{bcolors.RED}{message}{bcolors.ENDC}")
+
+def printError(message):
+    print(f"{bcolors.RED}{message}{bcolors.ENDC}")
